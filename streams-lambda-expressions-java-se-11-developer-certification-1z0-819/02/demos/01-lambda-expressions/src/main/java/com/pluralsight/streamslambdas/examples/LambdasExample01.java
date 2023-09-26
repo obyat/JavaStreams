1059259477506adc4e712573a105584b180d31cf
aobyat <rahman.obyat@gmail.com>
2023-09-25 23:16:49 -0400

    (JavaStreams) Update first example

--- streams-lambda-expressions-java-se-11-developer-certification-1z0-819/02/demos/01-lambda-expressions/src/main/java/com/pluralsight/streamslambdas/examples/LambdasExample01.java
+++ streams-lambda-expressions-java-se-11-developer-certification-1z0-819/02/demos/01-lambda-expressions/src/main/java/com/pluralsight/streamslambdas/examples/LambdasExample01.java
@@ -16,26 +16,28 @@ public class LambdasExample01 {
     public static void main(String[] args) {
         List<Product> products = ExampleData.getProducts();
 
-        // Interface Comparator implemented with an anonymous class.
-        products.sort(new Comparator<Product>() {
-            @Override
-            public int compare(Product p1, Product p2) {
-                return p1.getPrice().compareTo(p2.getPrice());
+        //Sort by directly implementing an anonymous class
+        //Sort in reverse ordering
+        products.sort(
+            new Comparator<Product>() {
+                @Override
+                public int compare(Product o1, Product o2) {
+                    return o2.getPrice().compareTo(o1.getPrice());
+                }
             }
-        });
+        );
 
-        // Interface Comparator implemented with a lambda expression.
-        products.sort((p1, p2) -> p1.getPrice().compareTo(p2.getPrice()));
+        //Sort by lambda
+        //Sort by lexicographic ordering
+        products.sort(
+            (p1, p2) -> p1.getPrice().compareTo(p2.getPrice())
+        );
 
-        // The same with a more verbose syntax:
-        // - You can optionally specify the type of the parameters
-        // - The body can be a block between { and } or a single expression
-        products.sort((Product p1, Product p2) -> {
-            return p1.getPrice().compareTo(p2.getPrice());
-        });
-
-        for (Product product : products) {
+        for(Product product: products){
             System.out.println(product);
         }
+
+
     }
 }
+
